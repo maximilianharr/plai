19f83f392334f87a01f374047493f978f8ce86d2
Maximilian Harr <maximilian.harr@gmail.com>
2021-02-07 18:53:31 +0100

    Added Dockerfile for tensorflow with Nvidia GPU support

---
+++ docker/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Greetings
+printf "${NC}--------------------------------------------------${NC}\n"
+printf "${RED}Hello $(whoami)! Welcome to $(hostname -s)!${NC}\n"
+printf "${NC}--------------------------------------------------${NC}\n"
+
+# Commands
+cd /home/$(whoami)/workspace
+source plai/setup.bash
+jupyter-notebook --port 8891 --ip 0.0.0.0 --NotebookApp.token='ai'
